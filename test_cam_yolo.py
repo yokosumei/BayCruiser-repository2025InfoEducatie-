48b768af579c6d5bf087d9277b424e64fae9c560
yoko <daria.antonia.stan@gmail.com>
2025-05-11 13:08:51 +0300

    Update test_cam_yolo.py

--- test_cam_yolo.py
+++ test_cam_yolo.py
@@ -2,8 +2,14 @@ from picamera2 import Picamera2
 import cv2
 
 picam2 = Picamera2()
-picam2.configure(picam2.create_preview_configuration())
+picam2.configure(picam2.create_preview_configuration(main={"format": 'BGR888', "size": (640, 480)}))
 picam2.start()
 
-frame = picam2.capture_array()
-cv2.imwrite("frame_test.jpg", frame)
+while True:
+    frame = picam2.capture_array()
+    cv2.imshow("Test Camera", frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cv2.destroyAllWindows()
+picam2.stop()
