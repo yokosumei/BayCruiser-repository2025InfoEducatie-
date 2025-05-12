a47bc840af16aab5ecabb41705eda6b485a3a3b5
yoko <daria.antonia.stan@gmail.com>
2025-05-12 11:18:02 +0300

    Update app.test.servo.py

--- test_servo/app.test.servo.py
+++ test_servo/app.test.servo.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from gpiozero.pins.pigpio import PiGPIOFactory
 from gpiozero import AngularServo
-from time import sleep
+
+factory = PiGPIOFactory()
+servo = AngularServo(18, min_pulse_width=0.0006, max_pulse_width=0.0023, pin_factory=factory)
+
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
 
-servo = AngularServo(18, min_pulse_width=0.0006, max_pulse_width=0.0023)
 
 @app.route("/")
 def home():
