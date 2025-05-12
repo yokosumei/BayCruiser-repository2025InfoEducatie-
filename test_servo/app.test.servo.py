fea862ac13b4b0a1b2b8b159d76b678396dd262a
yoko <daria.antonia.stan@gmail.com>
2025-05-12 10:43:44 +0300

    Update app.test.servo.py

--- test_servo/app.test.servo.py
+++ test_servo/app.test.servo.py
@@ -1,12 +1,18 @@
+from flask import Flask, render_template
 from gpiozero import AngularServo
 from time import sleep
 
+app = Flask(__name__, template_folder="templates", static_folder="static")
+
 servo = AngularServo(18, min_pulse_width=0.0006, max_pulse_width=0.0023)
 
-while (True):
-    servo.angle = 90
-    sleep(2)
+@app.route("/")
+def home():
+    return render_template("index.test.servo.html")
+
+@app.route("/misca")
+def misca_servo():
+    servo.angle = 110
+    sleep(1)
     servo.angle = 0
-    sleep(2)
-    servo.angle = -90
-    sleep(2)
+    return "Servo mișcat!"
