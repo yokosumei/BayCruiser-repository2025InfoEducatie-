258fb341373e3cdb152841a9a8094f31384eccd9
yokosumei <daria.antonia.stan@gmail.com>
2025-05-22 09:08:51 +0100

    Rezolvat

--- test_servo/app.test.servo.py
+++ test_servo/app.test.servo.py
@@ -1,4 +1,5 @@
-from flask import Flask
+
+from flask import Flask, render_template
 from gpiozero import AngularServo
 from time import sleep
 
@@ -10,11 +11,12 @@ servo1 = AngularServo(18, min_pulse_width=0.0006, max_pulse_width=0.0023)
 def index():
     return render_template("index.test.servo.html")
 
-@app.route("misca")
+@app.route("/misca")
 def activate():
     servo1.angle = 110
     sleep(2)
     servo1.angle = 90
-
+    return "Servo mișcat"
+    
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5001)
