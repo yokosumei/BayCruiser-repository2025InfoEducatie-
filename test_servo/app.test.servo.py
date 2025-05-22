766d212f18a475a79d75c6563e32aae1687751fa
yoko <daria.antonia.stan@gmail.com>
2025-05-22 10:41:37 +0300

    Update app.test.servo.py

--- test_servo/app.test.servo.py
+++ test_servo/app.test.servo.py
@@ -5,7 +5,6 @@ from time import sleep
 app = Flask(__name__)
 
 servo1 = AngularServo(18, min_pulse_width=0.0006, max_pulse_width=0.0023)
-servo2 = AngularServo(17, min_pulse_width=0.0006, max_pulse_width=0.0023)
 
 @app.route("/")
 def index():
@@ -14,10 +13,8 @@ def index():
 @app.route("misca")
 def activate():
     servo1.angle = 110
-    servo2.angle = 110
     sleep(2)
     servo1.angle = 90
-    servo2.angle = 90
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
