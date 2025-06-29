e8291793e0cacd260049f47047b0465b0adce01d
Daria Stan [dariuka] <daria.antonia.stan@gmail.com>
2025-06-29 21:18:21 +0300

    Update test_cam_yolo.py

--- test_cam_yolo.py
+++ test_cam_yolo.py
@@ -110,6 +110,34 @@ class DroneKitGPSProvider(BaseGPSProvider):
     print("Checking pre-arm conditions...")
     def __init__(self):
         self.vehicle = connect('/dev/ttyUSB0', baud=57600, wait_ready=False)
+    
+
+    connection_string = '/dev/ttyUSB0'
+    baud_rate = 57600
+    print("Connecting to vehicle...")
+    vehicle = connect(connection_string, baud=baud_rate, wait_ready=False)
+    
+    print("Checking pre-arm conditions...")
+    while not vehicle.is_armable:
+        print(" Waiting for vehicle to initialise...")
+        time.sleep(1)
+    print("Arming motors...")
+    vehicle.mode = VehicleMode("GUIDED")
+    vehicle.armed = True
+    while not vehicle.armed:
+        print(" Waiting for arming...")
+        time.sleep(1)
+    print("Taking off!")
+    vehicle.simple_takeoff(target_altitude)
+    while True:
+        alt = vehicle.location.global_relative_frame.alt
+        print(" Altitude: ", alt)
+        if alt >= target_altitude * 0.95:
+            print("Reached target altitude")
+            break
+        time.sleep(1)
+        
+        ////////////
         
         print("Checking pre-arm conditions...")
         while not self.vehicle.is_armable:
