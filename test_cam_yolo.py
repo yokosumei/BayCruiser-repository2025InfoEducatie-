4a996b79c696f172980d525e5db9a9af82103dd0
WALLE\daria <daria.antonia.stan@gmail.com>
2025-07-12 12:13:00 +0300

    add info livings_inference_thread

--- test_cam_yolo.py
+++ test_cam_yolo.py
@@ -573,10 +573,18 @@ def stream_thread():
         time.sleep(0.05)
 
 def livings_inference_thread(video=None):
+    logging.info("Firul livings_inference_thread a pornit.")
     global mar_output_frame, pose_triggered
     cap = cv2.VideoCapture(0) if video is None else cv2.VideoCapture(video)
     session = ort.InferenceSession("models/livings.onnx")
     input_name = session.get_inputs()[0].name
+
+
+    if os.path.exists("/models/livings.onnxt"):
+        print("Fișierul există!")
+    else:
+        print("Fișierul NU există.")
+    print("input_name:",input_name)
 
     while cap.isOpened():
         ret, frame = cap.read()
